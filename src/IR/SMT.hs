5be08af89d92cc51fa16e62a8b4f4ea05d09b918
mlfbrown <mlfbrown@stanford.edu>
2019-12-14 16:07:41 -0800

    more ported IR operations

--- src/IR/SMT.hs
+++ src/IR/SMT.hs
@@ -1,6 +1,7 @@
 module IR.SMT (
               ) where
-import           AST.Simple                 (Type (..), isDouble)
+import           AST.Simple                 (Type (..), isDouble, isSignedInt,
+                                             isUnsignedInt)
 import           Control.Monad
 import           Control.Monad.State.Strict
 import qualified Data.Map                   as M
@@ -44,12 +45,64 @@ newVar ty name = error ""
 --- IR operations for translating C++ to SMT
 ---
 
+-- Unary operations
+
+-- | C++ unary negation---meaning 5 becomes -5. This is not a bitwise negation
 cppNeg :: SMTNode -> SMT SMTNode
 cppNeg node = do
   let op = if isDouble $ t node then SMT.fpNeg else SMT.neg
-  result <- op (n node)
+  result <- op $ n node
   return $ mkNode result (t node) (u node)
 
+-- | C++ bitwise negation
+cppBitwiseNeg :: SMTNode -> SMT SMTNode
+cppBitwiseNeg node = do
+  when (isDouble $ t node) $ error "Cannot bitwise negate double"
+  result <- SMT.not $ n node
+  return $ mkNode result (t node) (u node)
+
+-- Comparisons
+
+-- | Wrapper for the C++ comparison operations.
+-- To: (1) make them polymorphic across types
+--     (2) choose the right kind of comparison depending on the input types
+--     (3) set the undef bit
+cppCompareWrapper :: SMTNode -- ^ Left operand
+                  -> SMTNode -- ^ Right operand
+                  -> (Node -> Node -> SMT Node) -- ^ Unsigned comparison op
+                  -> (Node -> Node -> SMT Node) -- ^ Signed comparison op
+                  -> (Node -> Node -> SMT Node) -- ^ Floating point comparison op
+                  -> SMT SMTNode -- ^ Result
+cppCompareWrapper left right uCompare sCompare fCompare
+ | isDouble (t left) || isDouble (t right) = do
+     unless (t left == t right) $ error "Expected two doubles as argumnets to comparison"
+     compare <- fCompare (n left) (n right)
+     maybeDefinedNode left right compare Bool
+ | isUnsignedInt (t left) || isUnsignedInt (t right) = do
+     compare <- uCompare (n left) (n right)
+     maybeDefinedNode left right compare Bool
+ | otherwise = do
+     compare <- sCompare (n left) (n right)
+     maybeDefinedNode left right compare Bool
+
+cppEq, cppGt, cppGte, cppLt, cppLte :: SMTNode -> SMTNode -> SMT SMTNode
+cppEq left right = cppCompareWrapper left right SMT.eq SMT.eq SMT.fpEq
+cppGt left right = cppCompareWrapper left right SMT.ugt SMT.sgt SMT.fpGt
+cppGte left right = cppCompareWrapper left right SMT.ugte SMT.sgte SMT.fpGte
+cppLt left right = cppCompareWrapper left right SMT.ult SMT.slt SMT.fpLt
+cppLte left right = cppCompareWrapper left right SMT.ulte SMT.slte SMT.fpLte
+
+-- Extra helpers
+
+-- | Make a new node that is defined if its parents are defined
+maybeDefinedNode :: SMTNode -- ^ Parent 1
+                 -> SMTNode -- ^ Parent 2
+                 -> Node -- ^ Inner SMT node
+                 -> Type -- ^ C++ type
+                 -> SMT SMTNode -- ^ Resulting IR node
+maybeDefinedNode parent1 parent2 node ty = do
+  childUndef <- SMT.or (u parent1) (u parent2)
+  return $ mkNode node ty childUndef
 
 ---
 --- IR operations for translating JS to SMT
