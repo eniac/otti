7db9b709caa499bcdc6495c43a1104c8a2de425a
mlfbrown <mlfbrown@stanford.edu>
2019-12-16 13:25:59 -0800

    compiler monad skeleton. adding ast builder functions so they can be attached to any parser

---
+++ src/Compiler/AST/SimpleBuilder.hs
@@ -0,0 +1,1 @@
+module Compiler.AST.SimpleBuilder where
--- src/Compiler/CompilerMonad.hs
+++ src/Compiler/CompilerMonad.hs
@@ -1,2 +1,24 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Compiler.CompilerMonad where
+import           Control.Monad.Fail
+import           Control.Monad.Reader
+import           Control.Monad.State.Strict
+import           Data.List                  (isInfixOf)
+import qualified Data.Map                   as M
+import           Targets.SMT
+import qualified Z3.Monad                   as Z
 
+-- | Internal state of the compiler for both translation to IR
+-- and for code generation.
+data CompilerState = CompilerState { vars :: M.Map String Int
+                                   }
+
+newtype Compiler a = Compiler (StateT CompilerState SMT a)
+    deriving (Functor, Applicative, Monad, MonadState CompilerState, MonadIO)
+
+instance Z.MonadZ3 Compiler where
+    getSolver = Compiler $ lift $ Z.getSolver
+    getContext = Compiler $ lift $ Z.getContext
+
+instance MonadFail Compiler where
+    fail = error "FAILED"
