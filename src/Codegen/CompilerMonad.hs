f70ffdb267daa1324b0a23fd0e1d55d15bcbcf33
mlfbrown <mlfbrown@stanford.edu>
2019-12-16 16:45:59 -0800

    all basic codegen state functions in place

--- src/Codegen/CompilerMonad.hs
+++ src/Codegen/CompilerMonad.hs
@@ -9,6 +9,18 @@ import qualified Data.Map                   as M
 import           Targets.SMT
 import qualified Z3.Monad                   as Z
 
+{-|
+
+Module that defines the Compiler monad, the monad that keeps track of all internal
+state for code generation. This state includes:
+WHAT
+
+There are many bits of low-hanging optimization fruit here. For example, we are doing
+redundant state lookups, and storing lists of function names instead of a hash of
+such a thing.
+
+-}
+
 type Version = Int
 -- | Variable name and de-ambiguating caller context information.
 -- This way, you can have two variables name 'foo' in different functions
@@ -17,14 +29,18 @@ type Version = Int
 data ASTVar = ASTVar VarName [FunctionName]
             deriving (Eq, Ord, Show)
 
+data CodegenVar = CodegenVar ASTVar Version
+                deriving (Eq, Ord, Show)
+
 -- | Internal state of the compiler for code generation
 data CompilerState = CompilerState { -- Mapping AST variables to information
-                                     vars             :: M.Map ASTVar Version
+                                     vers             :: M.Map ASTVar Version
                                    , tys              :: M.Map ASTVar Type
                                      -- Codegen context information
                                    , callStack        :: [FunctionName]
                                    , conditionalGuard :: Maybe Node
                                      -- SMT variables and memory
+                                   , vars             :: M.Map CodegenVar Node
                                    }
 
 newtype Compiler a = Compiler (StateT CompilerState SMT a)
@@ -42,7 +58,7 @@ instance MonadFail Compiler where
 ---
 
 emptyCompilerState :: CompilerState
-emptyCompilerState = CompilerState M.empty M.empty [] Nothing
+emptyCompilerState = CompilerState M.empty M.empty [] Nothing M.empty
 
 liftSMT :: SMT a -> Compiler a
 liftSMT = Compiler . lift
@@ -65,6 +81,9 @@ runSolverOnSMT = liftSMT runSolver
 ---
 ---
 
+-- Turning VarNames (the AST's representation of a variable) into other representations
+-- of variables
+
 -- | Take a VarName (the AST's representation of a variable) and turn it into an
 -- ASTVar---a variable that can be distinguished from other variables of the same
 -- name by its caller context.
@@ -73,6 +92,12 @@ astVar varName = do
   stack <- callStack `liftM` get
   return $ ASTVar varName stack
 
+codegenVar :: VarName -> Compiler CodegenVar
+codegenVar varName = do
+  var <- astVar varName
+  ver <- getVer varName
+  return $ CodegenVar var ver
+
 -- | Declare a new variable, or error if the variable is already declared.
 -- This adds the variable's version information (for SSA-ing) and type information
 -- to the compiler state.
@@ -80,20 +105,53 @@ declareVar :: VarName -> Type -> Compiler ()
 declareVar varName ty = do
   var <- astVar varName
   s0 <- get
+  let allVers = vers s0
+      allTys  = tys s0
+  case M.lookup var allVers of
+    Nothing -> put $ s0 { vers = M.insert var 0 allVers
+                        , tys = M.insert var ty allTys
+                        }
+    _       -> error $ unwords ["Already declared", varName, "in current scope"]
+
+-- | Bump the given variable up in version (for SSA)
+nextVer :: VarName -> Compiler ()
+nextVer varName = do
+  var <- astVar varName
+  s0 <- get
+  let allVers = vers s0
+  case M.lookup var allVers of
+    Just ver -> put $ s0 { vers = M.insert var (ver + 1) allVers }
+    _ -> error $ unwords ["Cannot increment version of undeclared", varName]
+
+-- | Get the current version of the variable
+getVer :: VarName -> Compiler Version
+getVer varName = do
+  var <- astVar varName
+  allVers <- vers `liftM` get
+  case M.lookup var allVers of
+    Just ver -> return ver
+    _        -> error $ unwords ["Cannot get version of undeclared", varName]
+
+-- | Get the C++ type of the variable
+getType :: VarName -> Compiler Type
+getType varName = do
+  var <- astVar varName
+  allTys <- tys `liftM` get
+  case M.lookup var allTys of
+    Just ty -> return ty
+    _       -> error $ unwords ["Cannot get type of undeclared", varName]
+
+-- | Get an SMT node representing the given var
+getNodeFor :: VarName -> Compiler Node
+getNodeFor varName = do
+  var <- codegenVar varName
+  s0 <- get
   let allVars = vars s0
-      allTys = tys s0
   case M.lookup var allVars of
-    Just{} -> error $ unwords ["Already declared", varName, "in current scope"]
-    _ -> put $ s0 { vars = M.insert var 0 allVars
-                  , tys = M.insert var ty allTys
-                  }
-
-nextVar :: VarName -> Compiler ()
-nextVar = undefined
-
-getVarNode :: VarName -> Compiler ()
-getVarNode name = undefined
-
-
-
+    Just node -> return node
+    Nothing -> do
+      ty <- getType varName
+      let node = undefined
+      put $ s0 { vars = M.insert var node allVars }
+      return node
 
