0f2d654e5dbb61d1db7ea8f1dd724bd1d1807d0d
mlfbrown <mlfbrown@stanford.edu>
2020-01-10 11:53:34 -0800

    parse wasm

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,12 +1,18 @@
 import           BenchUtils
 import           Codegen.SMTGenTest
 import           IR.SMTTest
+import           Parser.WASMTest
+import           Parser.WASMTest
 import           Targets.SMT
 import           Targets.SMTTest
 import           Test.Tasty
 
+parserTests :: BenchTest
+parserTests = benchTestGroup "Parser tests" [ wasmParserTests ]
+
 allTests :: [BenchTest]
-allTests = [ smtTests
+allTests = [ parserTests
+           , smtTests
            , irTests
            , codegenTests
            ]
