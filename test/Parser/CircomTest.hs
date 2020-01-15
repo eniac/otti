ceae0860ab0e795867758d3393591eba3f6923d5
Alex Ozdemir <aozdemir@hmc.edu>
2020-01-15 08:52:30 -0800

    Stray $'s

--- test/Parser/CircomTest.hs
+++ test/Parser/CircomTest.hs
@@ -12,10 +12,10 @@ circomParserTests = benchTestGroup "Circom tests"
     [ testLex "test/Code/Circom/inout.circom"
     , testExprParse "5" Nothing
     , testExprParse "x" $ Just $ LValue $ Ident "x"
-    , testExprParse "x + 5" $ Nothing
+    , testExprParse "x + 5" Nothing
     , testExprParse "in[j][k] * 2**k" Nothing --  https://github.com/iden3/circomlib/blob/master/circuits/binsum.circom#L77
     , testExprParse "2**k * in[j][k]" $ Just $ BinExpr Mul (BinExpr Pow (NumLit 2) (LValue (Ident "k"))) (LValue (Index (Index (Ident "in") (LValue (Ident "j"))) (LValue (Ident "k"))))
-    , testExprParse "2**(k * in[j][k])" $ Nothing
+    , testExprParse "2**(k * in[j][k])" Nothing
     , testExprParse "out[k] * (out[k] - 1)" Nothing --  https://github.com/iden3/circomlib/blob/master/circuits/binsum.circom#L85
     , testExprParse "[x, y, z + 5]" $ Just $ ArrayLit [LValue $ Ident "x", LValue $ Ident "y", BinExpr Add (LValue $ Ident "z") (NumLit 5)]
     , testExprParse "x + -x" $ Just $ BinExpr Add (LValue $ Ident "x") (UnExpr UnNeg (LValue $ Ident "x"))
