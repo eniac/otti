8079b830508b95f183236e8bb240d94091c905a6
Alex Ozdemir <aozdemir@hmc.edu>
2020-02-25 09:48:20 -0800

    Added new TySmt. Haven't fixed everything else yet.

--- src/IR/TySmt.hs
+++ src/IR/TySmt.hs
@@ -1,127 +1,47 @@
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# OPTIONS_GHC -Wall #-}
 {-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
-module IR.TySmt ( Sort(..)
-                , AnyTerm(..)
-                , BoolTerm(..)
-                , BoolNaryOp(..)
-                , BoolBinOp(..)
-                , IntTerm(..)
-                , IntNaryOp(..)
-                , IntBinOp(..)
-                , IntUnOp(..)
-                , IntBinPred(..)
-                , PfTerm(..)
-                , PfNaryOp(..)
-                , PfUnOp(..)
-                , PfBinPred(..)
-                , BvTerm(..)
-                , BvBinOp(..)
-                , BvBinPred(..)
-                , Term(..)
-                ) where
 
-import           Control.Monad.State.Strict (foldM, liftIO, unless)
+import GHC.TypeNats
 
-data Sort = Bv Int
-          | Pf Integer
-          | B
-          | Z
-          | Array String String
-          | Fp Int Int
-          deriving (Show, Ord, Eq)
+main :: IO ()
+main = do
+    print "Hi"
+    print $ show $ IntLit 5
+    print $ show $ IntBinExpr IntAdd (IntLit 5) (IntLit 6)
+    -- print $ show $ IntBinExpr IntAdd (IntLit 5) (BoolLit True) // ERROR!
+    print $ show $ BoolBinExpr And (BoolLit True) (BoolLit False)
+    print $ show $ NewArray
+    print $ show $ Store NewArray (IntLit 0) (BoolLit True)
+    print $ show $ Select (Store NewArray (IntLit 0) (BoolLit True)) (IntLit 0)
+    -- print $ show $ Select (Store NewArray (IntLit 0) (BoolLit True)) (BoolLit 0) // ERROR!
+    print $ show $ FpBinExpr FpAdd (Fp64Lit 3.4) (Var "a")
+    -- print $ show $ FpBinExpr FpAdd (Fp64Lit 3.4) (Var @F32 "a")
 
-sortFloat32 = Fp 8 24
-sortFloat64 = Fp 11 53
+data IntSort = IntSort deriving (Show)
+data BoolSort = BoolSort deriving (Show)
+data BvSort n = BvSort Int deriving (Show)
+data PfSort n = PfSort Integer deriving (Show)
+data FpSort n m = FpSort Int Int deriving (Show)
+data ArraySort k v = ArraySort deriving (Show)
 
-data AnyTerm = TermBool BoolTerm
-             | TermInt  IntTerm
-             | TermPf   PfTerm
-             | TermBv   BvTerm
-             | TermFp   FpTerm
-             -- TODO: TermArray?
-             deriving (Show, Ord, Eq)
+type F32 = FpSort 8 24
+type F64 = FpSort 11 53
 
--- Booleans
-data BoolTerm = BoolLit Bool
-              | BoolBinExpr BoolBinOp BoolTerm BoolTerm
-              | BoolNaryExpr BoolNaryOp [BoolTerm]
-              | BoolNeg BoolTerm
-              | IntPred IntBinPred IntTerm IntTerm
-              | PfPred PfBinPred PfTerm PfTerm
-              | BvBinPred BvBinPred BvTerm BvTerm
-              | FpBinPred FpBinPred FpTerm FpTerm
-              | FpUnPred FpUnPred FpTerm
-              | BoolVar String
-              | BoolExists String Sort BoolTerm
-              | BoolLet String AnyTerm BoolTerm
-              | BoolIte BoolTerm BoolTerm BoolTerm
-              -- TODO: Select?
-              deriving (Show, Ord, Eq)
+data Sort = SortInt
+          | SortBool
+          | SortBv Int
+          | SortPf Integer
+          | SortFp Int Int
+          | SortArray Sort Sort
+          deriving (Show)
 
-data BoolNaryOp = BoolAnd | BoolOr | BoolXor deriving (Show, Ord, Eq)
-data BoolBinOp = BoolImplies deriving (Show, Ord, Eq)
-
--- Arrays
-data ArrayTerm k v = Store (ArrayTerm k v) k v
-                   | ArrayVar String
-                   | ArrayExists String Sort (ArrayTerm k v)
-                   | ArrayLet String AnyTerm (ArrayTerm k v)
-
--- Integers
-data IntTerm = IntLit Integer
-             | IntNaryExpr IntBinOp [IntTerm]
-             | IntBinExpr IntBinOp IntTerm IntTerm
-             | IntUnExpr IntUnOp IntTerm
-             | PfToNat PfTerm
-             | BvToNat BvTerm
-             | BvToNatSigned BvTerm
-             | BoolToInt BoolTerm
-             | IntVar String
-             | IntExists String Sort IntTerm
-             | IntLet String AnyTerm IntTerm
-             | IntIte BoolTerm IntTerm IntTerm
-              -- TODO: Select?
-             deriving (Show, Ord, Eq)
-
-data IntNaryOp = IntAdd | IntMul
-               deriving (Show, Ord, Eq)
-data IntBinOp = IntSub | IntDiv | IntMod | IntShl | IntShr | IntPow
-              deriving (Show, Ord, Eq)
-data IntUnOp = IntNeg | IntAbs
-             deriving (Show, Ord, Eq)
-data IntBinPred = IntLt | IntLe | IntGt | IntGe | IntEq | IntNe
-                deriving (Show, Ord, Eq)
-
-
--- Prime Fields
-data PfTerm = PfLit Integer Integer -- order value
-            | PfNaryExpr PfNaryOp [PfTerm]
-            | PfUnExpr PfUnOp PfTerm
-            | IntToPf Integer IntTerm
-            | PfVar String
-            | PfExists String Sort PfTerm
-            | PfLet String AnyTerm PfTerm
-            | PfIte BoolTerm PfTerm PfTerm
-              -- TODO: Select?
-            deriving (Show, Ord, Eq)
-
-data PfNaryOp = PfAdd | PfMul deriving (Show, Ord, Eq)
-data PfUnOp = PfNeg | PfRecip deriving (Show, Ord, Eq)
-data PfBinPred = PfEq | PfNe deriving (Show, Ord, Eq)
-
--- Bitvectors
-data BvTerm = BvLit Int Integer
-            | BvBinExpr BvBinOp BvTerm BvTerm
-            | BvExtract Int Int BvTerm
-            | IntToBv Int IntTerm
-            | IntToBvSigned Int IntTerm
-            | BvVar String
-            | BvExists String Sort BvTerm
-            | BvLet String AnyTerm BvTerm
-            | BvIte BoolTerm BvTerm BvTerm
-            | FpToBv FpTerm
-            | FpToBvSigned FpTerm
-              -- TODO: Select?
-            deriving (Show, Ord, Eq)
+data BoolBinOp = And | Or | Xor | Implies deriving (Show)
 
 data BvBinOp = BvShl
              | BvLshr
@@ -134,8 +54,14 @@ data BvBinOp = BvShl
              | BvOr
              | BvAnd
              | BvXor
-             | BvConcat
              deriving (Show, Ord, Eq)
+
+data IntBinOp = IntSub | IntDiv | IntMod | IntShl | IntShr | IntPow | IntAdd | IntMul
+              deriving (Show, Ord, Eq)
+data IntUnOp = IntNeg | IntAbs
+             deriving (Show, Ord, Eq)
+data IntBinPred = IntLt | IntLe | IntGt | IntGe | IntEq | IntNe
+                deriving (Show, Ord, Eq)
 
 data BvBinPred = BvEq
                | BvNe
@@ -149,22 +75,9 @@ data BvBinPred = BvEq
                | BvSle
                deriving (Show, Ord, Eq)
 
--- Fp
-data FpTerm = FpFloatLit Float
-            | FpDoubleLit Double
-            | FpVar String
-            | FpFma FpTerm FpTerm FpTerm
-            | FpBinExpr FpBinOp FpTerm FpTerm
-            | FpUnExpr FpUnOp FpTerm
-            | BvToFp BvTerm Int Int
-            | BvToFpSigned BvTerm Int Int
-            | FpToFp BvTerm Int Int
-            | IntToFp IntTerm Int Int
-            | FpExists String Sort FpTerm
-            | FpLet String AnyTerm FpTerm
-            | FpIte BoolTerm FpTerm FpTerm
-            deriving (Show, Ord, Eq)
-
+data PfNaryOp = PfAdd | PfMul deriving (Show, Ord, Eq)
+data PfUnOp = PfNeg | PfRecip deriving (Show, Ord, Eq)
+data PfBinPred = PfEq | PfNe deriving (Show, Ord, Eq)
 
 data FpBinOp = FpAdd
              | FpSub
@@ -197,194 +110,58 @@ data FpUnPred = FpIsNormal
               | FpIsPositive
               deriving (Show, Ord, Eq)
 
-class Sorted a where
-    sort :: a -> Sort
 
-class Term a where
-    asAnyTerm :: a -> AnyTerm
-    children :: a -> [AnyTerm]
-    mapVar :: (String -> String) -> a -> a
+data Term s where
+    -- Boolean terms
+    BoolLit     :: Bool -> Term BoolSort
+    BoolBinExpr :: BoolBinOp -> Term BoolSort -> Term BoolSort -> Term BoolSort
+    Not         :: Term BoolSort -> Term BoolSort
 
-    depth :: a -> Int
-    depth t = 1 + foldr max 0 (map depth $ children t)
+    -- Core terms
+    Ite    :: Term BoolSort -> Term a -> Term a
+    Var    :: String -> Term s
+    Exists :: String -> Sort -> Term t -> Term t
 
-    allDescendants :: a -> [AnyTerm]
-    allDescendants t = asAnyTerm t : concatMap allDescendants (children t)
+    -- Bit-vector terms
+    BvConcat  :: (KnownNat n, KnownNat m) => Term (BvSort n) -> Term (BvSort m) -> Term (BvSort (n + m))
+    BvExtract :: (KnownNat n, KnownNat i, KnownNat j) => Term (BvSort n) -> Term (BvSort (j - i))
+    BvBinExpr :: KnownNat n => BvBinOp -> Term (BvSort n) -> Term (BvSort n) -> Term BoolSort
+    BvBinPred :: KnownNat n => BvBinPred -> Term (BvSort n) -> Term (BvSort n) -> Term BoolSort
+    IntToBv   :: KnownNat n => Term IntSort -> Term (BvSort n)
+    FpToBv    :: (KnownNat e, KnownNat s) => Term (FpSort e s) -> Term (BvSort (e + s))
 
-    asVariable :: a -> Maybe String
+    -- Integer terms
+    IntLit        :: Integer -> Term IntSort
+    IntUnExpr     :: IntUnOp -> Term IntSort -> Term BoolSort
+    IntBinExpr    :: IntBinOp -> Term IntSort -> Term IntSort -> Term BoolSort
+    IntBinPred    :: IntBinPred -> Term IntSort -> Term IntSort -> Term BoolSort
+    PfToInt       :: (KnownNat n) => Term (PfSort n) -> Term IntSort
+    BvToInt       :: (KnownNat n) => Term (BvSort n) -> Term IntSort
+    SignedBvToInt :: (KnownNat n) => Term (BvSort n) -> Term IntSort
+    BoolToInt     :: Term BoolSort -> Term IntSort
 
-instance Term AnyTerm where
-    asAnyTerm = id
-    children t = case t of
-        TermBv t   -> children t
-        TermFp t   -> children t
-        TermPf t   -> children t
-        TermBool t -> children t
-        TermInt t  -> children t
-    mapVar f t = case t of
-        TermBv t   -> TermBv   $ mapVar f t
-        TermPf t   -> TermPf   $ mapVar f t
-        TermFp t   -> TermFp   $ mapVar f t
-        TermBool t -> TermBool $ mapVar f t
-        TermInt t  -> TermInt  $ mapVar f t
-    asVariable t = case t of
-        TermBv t   -> asVariable t
-        TermPf t   -> asVariable t
-        TermFp t   -> asVariable t
-        TermBool t -> asVariable t
-        TermInt t  -> asVariable t
+    -- Floating point terms
+    Fp64Lit   :: Double -> Term F64
+    Fp32Lit   :: Float -> Term F32
+    FpUnExpr  :: (KnownNat e, KnownNat s) => FpUnOp -> Term (FpSort e s) -> Term BoolSort
+    FpBinExpr :: (KnownNat e, KnownNat s) => FpBinOp -> Term (FpSort e s) -> Term (FpSort e s) -> Term BoolSort
+    FpFma     :: (KnownNat e, KnownNat s) => FpBinOp -> Term (FpSort e s) -> Term (FpSort e s) -> Term (FpSort e s) -> Term (FpSort e s)
+    FpBinPred :: (KnownNat e, KnownNat s) => FpBinPred -> Term (FpSort e s) -> Term (FpSort e s) -> Term BoolSort
+    FpUnPred  :: (KnownNat e, KnownNat s) => FpUnPred -> Term (FpSort e s) -> Term BoolSort
+    IntToFp   :: (KnownNat e, KnownNat s) => Term IntSort -> Term (FpSort e s)
+    BvToFp    :: (KnownNat e, KnownNat s) => Term (BvSort (e + s)) -> Term (FpSort e s)
+    FpToFp    :: (KnownNat e1, KnownNat s1, KnownNat e2, KnownNat s2) => Term (FpSort e1 s1) -> Term (FpSort e2 s2)
 
-instance Term BvTerm where
-    asAnyTerm = TermBv
-    children t = case t of
-        BvLit {}          -> []
-        BvBinExpr _ a b   -> [asAnyTerm a, asAnyTerm b]
-        BvExtract _ _ a   -> [asAnyTerm a]
-        IntToBv _ a       -> [asAnyTerm a]
-        IntToBvSigned _ a -> [asAnyTerm a]
-        FpToBv a          -> [asAnyTerm a]
-        FpToBvSigned a    -> [asAnyTerm a]
-        BvVar {}          -> []
-        BvExists _ _ a    -> [asAnyTerm a]
-        BvLet _ a b       -> [a, asAnyTerm b]
-        BvIte c t f       -> [asAnyTerm c, asAnyTerm t, asAnyTerm f]
-    mapVar f t = case t of
-        l@BvLit {}        -> l
-        BvBinExpr o a b   -> BvBinExpr o (mapVar f a) (mapVar f b)
-        BvExtract i j a   -> BvExtract i j (mapVar f a)
-        IntToBv i a       -> IntToBv i (mapVar f a)
-        IntToBvSigned i a -> IntToBvSigned i (mapVar f a)
-        FpToBv a          -> FpToBv (mapVar f a)
-        FpToBvSigned a    -> FpToBvSigned (mapVar f a)
-        BvVar s           -> BvVar (f s)
-        BvExists s l a    -> BvExists (f s) l (mapVar f a)
-        BvLet s a b       -> BvLet (f s) (mapVar f a) (mapVar f b)
-        BvIte c t ff      -> BvIte (mapVar f c) (mapVar f t) (mapVar f ff)
-    asVariable t = case t of
-        BvVar s -> Just s
-        _       -> Nothing
+    -- Prime field terms
+    PfUnExpr   :: KnownNat n => PfUnOp -> Term (PfSort n) -> Term BoolSort
+    PfNaryExpr :: KnownNat n => PfNaryOp -> [Term (PfSort n)] -> Term BoolSort
+    PfBinPred  :: KnownNat n => PfBinPred -> Term (PfSort n) -> Term (PfSort n) -> Term BoolSort
+    IntToPf    :: KnownNat n => Term IntSort -> Term (PfSort n)
 
-instance Term FpTerm where
-    asAnyTerm = TermFp
-    children t = case t of
-        FpFloatLit _       -> []
-        FpDoubleLit _      -> []
-        FpVar _            -> []
-        FpFma a b c        -> map asAnyTerm [a, b, c]
-        FpBinExpr _ a b    -> map asAnyTerm [a, b]
-        FpUnExpr _ a       -> [asAnyTerm a]
-        BvToFp a _ _       -> [asAnyTerm a]
-        BvToFpSigned a _ _ -> [asAnyTerm a]
-        FpToFp a _ _       -> [asAnyTerm a]
-        IntToFp a _ _      -> [asAnyTerm a]
-        FpExists _ _ a     -> [asAnyTerm a]
-        FpLet _ a b        -> [a, asAnyTerm b]
-        FpIte c t f        -> [asAnyTerm c, asAnyTerm t, asAnyTerm f]
-    mapVar f t = case t of
-        FpFloatLit l       -> t
-        FpDoubleLit l      -> t
-        FpVar s            -> FpVar $ f s
-        FpFma a b c        -> FpFma (mapVar f a) (mapVar f b) (mapVar f c)
-        FpBinExpr o a b    -> FpBinExpr o (mapVar f a) (mapVar f b)
-        FpUnExpr o a       -> FpUnExpr o (mapVar f a)
-        BvToFp a e s       -> BvToFp (mapVar f a) e s
-        BvToFpSigned a e s -> BvToFpSigned (mapVar f a) e s
-        FpToFp a e s       -> FpToFp (mapVar f a) e s
-        IntToFp a e s      -> IntToFp (mapVar f a) e s
-        FpExists s l a     -> FpExists (f s) l (mapVar f a)
-        FpLet s a b        -> FpLet (f s) (mapVar f a) (mapVar f b)
-        FpIte c t ff       -> FpIte (mapVar f c) (mapVar f t) (mapVar f ff)
-    asVariable t = case t of
-        FpVar s -> Just s
-        _       -> Nothing
-
-instance Term IntTerm where
-    asAnyTerm = TermInt
-    children t = case t of
-        IntLit {}        -> []
-        IntNaryExpr _ as -> map asAnyTerm as
-        IntBinExpr _ a b -> [asAnyTerm a, asAnyTerm b]
-        IntUnExpr _ a    -> [asAnyTerm a]
-        BvToNat a        -> [asAnyTerm a]
-        BvToNatSigned a  -> [asAnyTerm a]
-        PfToNat a        -> [asAnyTerm a]
-        IntVar {}        -> []
-        IntExists _ _ a  -> [asAnyTerm a]
-        IntLet _ a b     -> [a, asAnyTerm b]
-        BoolToInt a      -> [asAnyTerm a]
-        IntIte c t f     -> [asAnyTerm c, asAnyTerm t, asAnyTerm f]
-    mapVar f t = case t of
-        l@IntLit {}      -> l
-        IntNaryExpr o as -> IntNaryExpr o (map (mapVar f) as)
-        IntBinExpr o a b -> IntBinExpr o (mapVar f a) (mapVar f b)
-        IntUnExpr o a    -> IntUnExpr o (mapVar f a)
-        BvToNat a        -> BvToNat (mapVar f a)
-        BvToNatSigned a  -> BvToNatSigned (mapVar f a)
-        PfToNat a        -> PfToNat (mapVar f a)
-        IntVar s         -> IntVar (f s)
-        IntExists s l a  -> IntExists (f s) l (mapVar f a)
-        IntLet s a b     -> IntLet (f s) (mapVar f a) (mapVar f b)
-        BoolToInt a      -> BoolToInt (mapVar f a)
-        IntIte c t ff    -> IntIte (mapVar f c) (mapVar f t) (mapVar f ff)
-    asVariable t = case t of
-        IntVar s -> Just s
-        _        -> Nothing
-
-instance Term PfTerm where
-    asAnyTerm = TermPf
-    children t = case t of
-        PfLit {}        -> []
-        PfNaryExpr _ as -> map asAnyTerm as
-        PfUnExpr _ a    -> [asAnyTerm a]
-        IntToPf _ a     -> [asAnyTerm a]
-        PfVar {}        -> []
-        PfExists _ _ a  -> [asAnyTerm a]
-        PfLet _ a b     -> [a, asAnyTerm b]
-        PfIte c t f     -> [asAnyTerm c, asAnyTerm t, asAnyTerm f]
-    mapVar f t = case t of
-        l@PfLit {}      -> l
-        PfNaryExpr o as -> PfNaryExpr o (map (mapVar f) as)
-        PfUnExpr o a    -> PfUnExpr o (mapVar f a)
-        IntToPf i a     -> IntToPf i (mapVar f a)
-        PfVar s         -> PfVar (f s)
-        PfExists s l a  -> PfExists (f s) l (mapVar f a)
-        PfLet s a b     -> PfLet (f s) (mapVar f a) (mapVar f b)
-        PfIte c t ff    -> PfIte (mapVar f c) (mapVar f t) (mapVar f ff)
-    asVariable t = case t of
-        PfVar s -> Just s
-        _       -> Nothing
+    -- Array terms
+    Select   :: (Show k, Show v) => Term (ArraySort k v) -> Term k -> Term v
+    Store    :: (Show k, Show v) => Term (ArraySort k v) -> Term k -> Term v -> Term (ArraySort k v)
+    NewArray :: Term (ArraySort k v)
 
 
-instance Term BoolTerm where
-    asAnyTerm = TermBool
-    children t = case t of
-        BoolLit {}        -> []
-        BoolNaryExpr _ as -> map asAnyTerm as
-        BoolBinExpr _ a b -> [asAnyTerm a, asAnyTerm b]
-        BoolNeg a         -> [asAnyTerm a]
-        BoolVar {}        -> []
-        BoolExists _ _ a  -> [asAnyTerm a]
-        BoolLet _ a b     -> [a, asAnyTerm b]
-        IntPred _ a b     -> [asAnyTerm a, asAnyTerm b]
-        PfPred _ a b      -> [asAnyTerm a, asAnyTerm b]
-        BvBinPred _ a b   -> [asAnyTerm a, asAnyTerm b]
-        FpBinPred _ a b   -> [asAnyTerm a, asAnyTerm b]
-        FpUnPred _ a      -> [asAnyTerm a]
-        BoolIte c t f     -> [asAnyTerm c, asAnyTerm t, asAnyTerm f]
-    mapVar f t = case t of
-        l@BoolLit {}      -> l
-        BoolNaryExpr o as -> BoolNaryExpr o (map (mapVar f) as)
-        BoolBinExpr o a b -> BoolBinExpr o (mapVar f a) (mapVar f b)
-        BoolNeg a         -> BoolNeg (mapVar f a)
-        BoolVar s         -> BoolVar (f s)
-        BoolExists s l a  -> BoolExists (f s) l (mapVar f a)
-        BoolLet s a b     -> BoolLet (f s) (mapVar f a) (mapVar f b)
-        IntPred p a b     -> IntPred p (mapVar f a) (mapVar f b)
-        PfPred p a b      -> PfPred p (mapVar f a) (mapVar f b)
-        BvBinPred p a b   -> BvBinPred p (mapVar f a) (mapVar f b)
-        FpBinPred p a b   -> FpBinPred p (mapVar f a) (mapVar f b)
-        FpUnPred p a      -> FpUnPred p (mapVar f a)
-        BoolIte c t ff    -> BoolIte (mapVar f c) (mapVar f t) (mapVar f ff)
-    asVariable t = case t of
-        BoolVar s -> Just s
-        _         -> Nothing
+deriving instance Show s => Show (Term s)
