d6fee2611cfad2861df6bb88277b1c876cb1ed8d
Alex Ozdemir <aozdemir@hmc.edu>
2020-12-18 14:42:10 -0800

    fractional labels

--- eval/zokrates/plot.R
+++ eval/zokrates/plot.R
@@ -10,6 +10,6 @@ ggplot(data = dw) +
   labs(x = "Benchmark",
        y = "Constraint Ratio\nCirC/ZoKrates\n(lower is better)") +
   geom_hline(yintercept=1) +
-  scale_y_continuous(trans="log2", limits=c(0.5,2)) +
+  scale_y_continuous(trans="log2", limits=c(0.5,2), labels=c("1/2","1", "2"), breaks = c(.5,1,2)) +
   t
 ggsave("results.png", width = 3, height = 2, units = "in")
