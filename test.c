9aa825e16b6cfe6c77500cc3c7749b7ff0ca87ad
jkwoods <jesskwoods@gmail.com>
2022-10-01 17:47:11 -0400

    clean up, submod

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdbool.h>
-
-int main(__attribute__((public(0))) int w0, __attribute__((public(0))) int w1, __attribute__((public(0))) int w2) {
-
-
-    int check = (3-2)*w0;
-
-    return check;
-}
